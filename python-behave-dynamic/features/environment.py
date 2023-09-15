8cfcc37bfd1abe3a6bcebf0240e945c943420bed
Karthick Nanmaran <karthick.nanmaran@pariplaydev.com>
2023-09-15 22:07:09 +0530

    simplified code

--- python-behave-dynamic/features/environment.py
+++ python-behave-dynamic/features/environment.py
@@ -4,9 +4,7 @@ import copy
 def before_feature(context, feature):
     input_num = context.config.userdata.get('input_num', '')
     numbers = input_num.split(",")
-    print(numbers)
-    features = (s for s in feature.scenarios if type(s) == behave.model.ScenarioOutline and
-                'dynamic' in s.tags)
+    features = (s for s in feature.scenarios if type(s) == behave.model.ScenarioOutline and 'dynamic' in s.tags)
     for s in features:
         for e in s.examples:
             orig = copy.deepcopy(e.table.rows[0])
